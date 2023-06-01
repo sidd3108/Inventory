7349e8b08eff95af050a02ea9a6843054d0d8233
Aniketh Pratihar <102458123+ANIKETH232323@users.noreply.github.com>
2023-06-01 11:05:22 +0530

    Update User.java

--- app/src/main/java/com/example/inventory/User.java
+++ app/src/main/java/com/example/inventory/User.java
@@ -3,8 +3,7 @@ package com.example.inventory;
 public class User {
     public String product_name,category,price,itembarcode;
 
-    public User() {
-    }
+    public User() { }
     public User(String product_name, String category, String price,String itembarcode) {
         this.product_name = product_name;
         this.category = category;
