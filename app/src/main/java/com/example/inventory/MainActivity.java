1be9ac9be77429e5f86c98eafca5e131f2ec0f2e
Aniketh Pratihar <aniketh5660@gmail.com>
2023-06-09 10:06:20 +0530

    Done

--- app/src/main/java/com/example/inventory/MainActivity.java
+++ app/src/main/java/com/example/inventory/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
